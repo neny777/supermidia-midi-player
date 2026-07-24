89b0455b0a8143ab331e11e1f8ba68fa70e73496
Denis Antonio Rocha <denisantoniorocha@gmail.com>
2026-07-24 18:11:33 -0300

    docs: atualizar informações da janela sobre

--- src/main/java/br/com/supermidia/app/MainController.java
+++ src/main/java/br/com/supermidia/app/MainController.java
@@ -288,7 +288,7 @@ public final class MainController {
         about.setTitle("Sobre o SuperMídia MIDI Player");
         about.setHeaderText("SuperMídia MIDI Player · versão 0.1.0");
         about.setContentText("Player MIDI desenvolvido para apresentações ao vivo.\n\n"
-                + "Site: https://www.supermidiaalfenas.com.br\n"
+                + "Desenvolvedor: Denis Antonio Rocha\n"
                 + "Projeto: https://github.com/neny777/supermidia-midi-player\n\n"
                 + "Tipografia Source Sans 3 · licença OFL 1.1");
         about.initOwner(currentSongLabel.getScene().getWindow());
